f12ad3ddb8038e908af2c58ffb318a6b5c3e8cad
agray998 <a.gray.4797@gmail.com>
2022-07-26 11:41:31 +0000

    added templates

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,17 @@
-from flask import request, redirect, url_for
+from flask import request, redirect, url_for, render_template
 from application import app, db
 from application.models import *
 from datetime import date
 
+def age(customer):
+    return (date.today() - customer.dob).days // 365
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
 # CRUD - create, read, update, delete
+# Create
 @app.route('/add-customer/<forename>-<surname>/<address>/<dob>/<email>')
 def create_customer(forename, surname, address, dob, email):
     date_values = map(int, dob.split('-'))
@@ -13,10 +21,11 @@ def create_customer(forename, surname, address, dob, email):
     db.session.commit()
     return "added customer"
 
+# Read
 @app.route('/get-customers')
 def view_customers():
-    customers = map(str, Customer.query.order_by(Customer.forename).all())
-    return '<br>'.join(customers)
+    customers = Customer.query.order_by(Customer.forename).all()
+    return render_template('view_all_customers.html', customers=list(customers))
 
 @app.route('/get-cust-by-name/<name>')
 def view_custs_by_name(name):
@@ -26,8 +35,32 @@ def view_custs_by_name(name):
 @app.route('/get-cust-by-id/<int:id>')
 def get_cust_by_id(id):
     customer = Customer.query.get(id)
-    return str(customer)
+    return render_template('cust_by_id.html', customer=customer)
 
 @app.route('/num-customers')
 def find_num_custs():
     return str(Customer.query.count())
+
+@app.route('/avg-age')
+def avg_age():
+    customers = Customer.query.all()
+    customer_ages = map(age, customers)
+    return str(sum(customer_ages) // Customer.query.count())
+
+# update
+@app.route('/update-customer/<int:id>/<attribute>/<new_val>')
+def update_cust(id, attribute, new_val):
+    customer_to_update = Customer.query.get(id)
+    if attribute == 'dob':
+        new_val = date(*(map(int, new_val.split('-'))))
+    setattr(customer_to_update, attribute, new_val)
+    db.session.commit()
+    return redirect(url_for('view_customers'))
+
+# delete
+@app.route('/delete-customer/<int:id>')
+def delete_customer(id):
+    cust_to_delete = Customer.query.get(id)
+    db.session.delete(cust_to_delete)
+    db.session.commit()
+    return redirect(url_for('view_customers'))
